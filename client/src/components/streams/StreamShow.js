99d91348b2135783baeea61bcd8c7f3c4b6e3548
PrateekAdhikaree <soumyapadhikaree@gmail.com>
2020-05-21 11:29:03 -0400

    stream show page scaffolding

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -1,7 +1,31 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-const StreamShow = () => {
-  return <div>StreamShow</div>;
+import { fetchStream } from '../../actions';
+
+class StreamShow extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+
+  render() {
+    if (!this.props.stream) {
+      return <div>Loading...</div>;
+    }
+
+    const { title, description } = this.props.stream;
+
+    return (
+      <div>
+        <h1>{title}</h1>
+        <h5>{description}</h5>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  return { stream: state.streams[ownProps.match.params.id] };
 };
 
-export default StreamShow;
+export default connect(mapStateToProps, { fetchStream })(StreamShow);
